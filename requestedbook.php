b200c892700f7ffc578fbdd97b3565d909310c46
nileshd1 <nileshdarade36@gmail.com>
2024-06-29 15:52:09 +0530

    library Management system using php

---
+++ requestedbook.php
@@ -0,0 +1,57 @@
+<?php
+include("setting.php");
+session_start();
+if(!isset($_SESSION['sid']))
+{
+	header("location:index.php");
+}
+$studid=$_SESSION['sid'];
+$a=mysqli_query($set,"SELECT * FROM students WHERE sid='$studid'");
+$b=mysqli_fetch_array($a);
+$name=$b['name'];
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Library Management System</title>
+<link href="stylesheet.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="banner">
+<span class="head">Library Management System</span><br />
+
+</div>
+<br />
+
+<div align="center">
+<div id="wrapper">
+<br />
+<br />
+
+<span class="SubHead">Books Requested</span>
+<br />
+<br />
+
+<table border="0" class="table" cellpadding="10" cellspacing="10">
+<tr class="labels" style="text-decoration:underline;"><th>Book Name</th><th>Author</th></tr>
+<?php
+$x=mysqli_query($set,"SELECT * FROM request where sid=$studid");
+while($y=mysqli_fetch_array($x))
+{
+	?>
+<tr class="labels" style="font-size:14px;"><td><?php echo $y['name'];?></td><td><?php echo $y['author'];?></td></tr>
+<?php
+}
+?>
+</table><br />
+<br />
+<a href="adminhome.php" class="link">Go Back</a>
+<br />
+<br />
+
+</div>
+</div>
+</body>
+</html>
